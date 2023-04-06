f6ac399e24751adb639fc0835f1aab9e36a0331d
Lucas chagas <lucazchagaz@outlook.com>
2023-04-06 10:49:20 -0300

    to add api axios calls

--- App_Cleint/src/api/api.tsx
+++ App_Cleint/src/api/api.tsx
@@ -2,9 +2,22 @@ import { View, Text } from 'react-native';
 import React from 'react';
 import axios from 'axios';
 
+import useRecorder from '../Hooks/UseRecorder/useRecorder';
+
+import { useNavigation } from '@react-navigation/native';
+
 export default function api() {
   // CHAMADAS API VIA AXIOS IRAO AQUI...
   // ESPERANDO A FINALIZAÇÃO DO SERVER
+
+  const navigation = useNavigation();
+
+  const { loading, setLoading, setErrorMessage, errorMessage } = useRecorder();
+
+  const err = () => {
+    setLoading(false);
+    setErrorMessage({ message: 'Ops, parece que tem algo de errado com o ChatGPT', status: true });
+  };
 
   return (
     <View>
