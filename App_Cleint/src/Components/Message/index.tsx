e6bcd97949ca4335e2ed351db58164e682ba46c6
Lucas chagas <lucazchagaz@outlook.com>
2023-05-04 14:30:24 -0300

    update message styles file

--- App_Cleint/src/Components/Message/index.tsx
+++ App_Cleint/src/Components/Message/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef } from 'react';
-import { Animated } from 'react-native';
+import { Animated, StyleSheet } from 'react-native';
 import { Icon, MessageText } from './styles';
 
 type IMessageProps = {
@@ -33,24 +33,25 @@ export default function Message({ label }: IMessageProps) {
   }, []);
 
   return (
-    <Animated.View
-      style={{
-        opacity: animet,
-        backgroundColor: '#FFF',
-        width: '100%',
-        height: 70,
-        flexDirection: 'row',
-        position: 'absolute',
-        top: 10,
-        alignItems: 'center',
-        justifyContent: 'center',
-        borderRadius: 10,
-        borderLeftWidth: 7,
-        borderLeftColor: '#ce1919',
-      }}
-    >
+    <Animated.View style={[styles.Conteiner, { opacity: animet }]}>
       <Icon color="#ce1919" name="times-circle-o" size={35} style={{ marginLeft: 10 }} />
       <MessageText>{label}</MessageText>
     </Animated.View>
   );
 }
+
+const styles = StyleSheet.create({
+  Conteiner: {
+    backgroundColor: '#FFF',
+    width: '100%',
+    height: 70,
+    flexDirection: 'row',
+    position: 'absolute',
+    top: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 10,
+    borderLeftWidth: 7,
+    borderLeftColor: '#ce1919',
+  },
+});
--- App_Cleint/src/Components/Message/styles.ts
+++ App_Cleint/src/Components/Message/styles.ts
@@ -1,31 +1,7 @@
 import styled from 'styled-components/native';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
-type IConteinerProps = {
-  type: 'success' | 'error' | 'alert';
-};
-
-export const Conteiner = styled.View<IConteinerProps>`
-  position: absolute;
-  top: 10px;
-  width: 100%;
-  max-height: 60px;
-  height: 60px;
-  padding: 10px;
-  align-items: center;
-  justify-content: space-between;
-  background-color: #f1f1f1;
-  flex-direction: row;
-  border-radius: 5px;
-  border-left-width: 7px;
-  border-color: ${({ type }) =>
-    type === 'success' ? '#0c9e1d' : type === 'error' ? '#ce1919' : '#6aa9f2'};
-  /* transition: transform 5s ease-in; */
-`;
-
 export const Icon = styled(FontAwesome)``;
-
-export const Button = styled.TouchableOpacity``;
 
 export const MessageText = styled.Text`
   flex: 1;
