6f55d2289f5b821bba592dcbe2ce5cc06afcffd3
Lucas chagas <lucazchagaz@outlook.com>
2023-05-04 14:31:17 -0300

    change api function

---
+++ App_Cleint/src/services/api.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import axios from 'axios';
+import { useNavigation } from '@react-navigation/native';
+
+import { useRecorderContext } from '../Context/RecorderContext';
+
+export default function Api() {
+  const navigation = useNavigation();
+
+  const { setErrorMessage, setLoading } = useRecorderContext();
+
+  const sendQuestion = async (message: string) => {
+    console.log(message, 'Pergunta');
+
+    try {
+      const res = await axios.post(
+        'http://10.0.0.104:8000/api/transcribe',
+        { text: message },
+        {
+          headers: {
+            'Accept-Encoding': 'identity',
+          },
+        }
+      );
+
+      const data = await res.data;
+      console.log(data, 'RESPOSTA');
+      setLoading(false);
+      navigation.navigate('RecordPlayer', {
+        dados: data,
+        question: message,
+      });
+    } catch (error) {
+      setErrorMessage({
+        message: 'erro ao acessar a Api do chatGPT, tente novamente...',
+        status: true,
+      });
+      navigation.navigate('Home');
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  return { sendQuestion };
+}
